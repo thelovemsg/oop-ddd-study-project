d5f34161345b395447973fbf5e949ac2e1bce7c3
thelovemsg <thelovemsg@naver.com>
2026-02-22 15:49:25 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/com/example/oopdddstudyproject/OopDddStudyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.oopdddstudyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopDddStudyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OopDddStudyProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/oopdddstudyproject/OopDddStudyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oopdddstudyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopDddStudyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
